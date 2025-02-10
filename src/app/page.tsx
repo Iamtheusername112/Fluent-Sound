3b377e21a8011a0af88204702f5566af24c39900
IamfrancisIwu <Francis.Iwu@teclead-ventures.de>
2025-02-10 13:03:44 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,81 @@
-import Image from "next/image";
+'use client'
+
+import { useState } from 'react'
+import MaxWidthWrapper from '@/components/common/MaxWidthWrapper'
+import { Badge } from '@/components/ui/badge'
+import { buttonVariants } from '@/components/ui/button'
+import { ChevronRight, Play, Sparkles } from 'lucide-react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <MaxWidthWrapper className='bg-background text-foreground'>
+      <section className='flex flex-col-reverse lg:flex-row items-center justify-between py-20 px-6 gap-10'>
+        <div className='max-w-xl text-center space-y-6 lg:text-left'>
+          <Badge variant={'secondary'} className='mb-4'>
+            <Sparkles className='w-3 h-3 mr-1' />
+            AI Powered Learning
+          </Badge>
+          <h1 className='text-4xl fonrt-bold tracking-tight leading-tight sm:text-6xl'>
+            Master any language through{' '}
+            <span className='text-primary relative'>
+              Active listening
+              <div className='absolute bottom-0 w-full h-1 rounded-full bg-primary/20'></div>
+            </span>
+          </h1>
+          <p className='text-lg text-muted-foreground'>
+            Transform your language learning journey with AI-powered exercises.
+          </p>
+          <div className='flex flex-col gap-4 pt-4 sm:flex-row'>
+            <Link
+              href={'/about'}
+              className={buttonVariants({ variant: 'default' })}
+            >
+              Get Started
+              <ChevronRight className='w-4 h-4 ml-2' />
+            </Link>
+
+            <Link
+              href={'/about'}
+              className={buttonVariants({ variant: 'outline' })}
+            >
+              Learn More
+              <Play className='w-4 h-4 ml-2' />
+            </Link>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        <div className='relative w-full lg:w-1/2'>
+          <div className='relative w-full aspect-video rounded-xl overflow-hidden shadow-2xl'>
+            {isVideoPlaying ? (
+              <iframe
+                className='w-full h-full'
+                src='https://www.youtube.com/embed/Ahwoks_dawU?autoplay=1'
+                title='YouTube video player'
+                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                allowFullScreen
+              ></iframe>
+            ) : (
+              <div
+                className='relative w-full h-full bg-black cursor-pointer'
+                onClick={() => setIsVideoPlaying(true)}
+              >
+                <Image
+                  src='https://img.youtube.com/vi/Ahwoks_dawU/hqdefault.jpg'
+                  alt='Video Thumbnail'
+                  layout='fill'
+                  objectFit='cover'
+                />
+                <div className='absolute inset-0 bg-black/50 flex items-center justify-center'>
+                  <Play className='w-16 h-16 text-white' />
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
+    </MaxWidthWrapper>
+  )
 }
